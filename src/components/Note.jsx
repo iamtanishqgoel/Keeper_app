4f2b69ce052df3fa1767ce8bb8416fd2eb2b4e6e
iamtanishqgoel <tanishqg2704@gmail.com>
2024-03-19 00:42:57 +0530

    deploy commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <div className="end">
+      <label>{props.date}</label>
+      <button onClick={handleClick}>
+      <DeleteIcon />
+      </button>
+      </div>
+    </div>
+  );
+}
+
+export default Note;
